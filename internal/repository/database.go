dca698b1ca7eda57ad7dbd17f143ce90037f1baa
Kevin Rosengren <rosenkev4@gmail.com>
2025-11-26 17:45:34 -0600

    Added database func to query all of the comments on a post

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
 )
 
@@ -44,7 +45,7 @@ func (db *DB) GetAllComments() ([]model.Comment, error) {
 
 	rows, err := db.Query(query)
 	if err != nil {
-		return nil, fmt.Errorf("failed to query comments")
+		return nil, fmt.Errorf("failed to query comments: %w", err)
 	}
 	defer rows.Close()
 
@@ -60,6 +61,30 @@ func (db *DB) GetAllComments() ([]model.Comment, error) {
 	}
 
 	return commentsList, nil
+}
+
+// GET api/post/{postId}/comments - Get all comments on a post
+func (db *DB) GetCommentsByPost(postId int) ([]model.Comment, error) {
+	query := "SELECT * FROM comments WHERE post_id = $1"
+
+	rows, err := db.Query(query, postId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query comments on post: %w", err)
+	}
+	defer rows.Close()
+
+	var commentList []model.Comment
+	for rows.Next() {
+		var comment model.Comment
+		err := rows.Scan(&comment.CommentId, &comment.UserId, &comment.PostId, &comment.Content, &comment.Author, &comment.DatePosted)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan comments on post")
+		}
+
+		commentList = append(commentList, comment)
+	}
+
+	return commentList, nil
 }
 
 // #endregion
