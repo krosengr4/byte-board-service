c9f43a56782a2f33cbcc283ab18bd460e08e6cb5
Kevin Rosengren <rosenkev4@gmail.com>
2025-11-26 04:39:27 -0600

    Set up handlers file with logic to create new instance of a handler, and methods to write responses and error responses

---
+++ internal/handler/handlers.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"byte-board/internal/appconfig"
+	"byte-board/internal/repository"
+	"encoding/json"
+	"net/http"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Handler struct {
+	db     *repository.DB
+	config *appconfig.Config
+}
+
+// Create a new instance of a handler
+func New(db *repository.DB, cfg *appconfig.Config) *Handler {
+	return &Handler{
+		db:     db,
+		config: cfg,
+	}
+}
+
+// Represents an error response
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+// Writes a JSON response
+func writeJSONResponse(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Error().Err(err).Msg("Error encoding JSON response")
+	}
+}
+
+// Writes an error response
+func writeErrorResponse(w http.ResponseWriter, status int, message string) {
+	log.Warn().Int("status", status).Str("message", message).Msg("Writing error response")
+	writeJSONResponse(w, status, ErrorResponse{Error: message})
+}
