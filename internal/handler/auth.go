467c5c25bdd3879ecd61ff73fac538a83793733e
Kevin Rosengren <rosenkev4@gmail.com>
2025-12-08 05:27:39 -0600

    Added logic for a existing user to login

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -65,3 +65,55 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 
 	writeJSONResponse(w, http.StatusOK, response)
 }
+
+// POST /api/login - Login handler
+func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
+	log.Info().Msg("POST /api/login - User attempting to login")
+
+	// Parse body request
+	var req model.LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Warn().Err(err).Msg("Invalid request body")
+		writeErrorResponse(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+
+	// Validate required fields
+	if req.Username == "" || req.Password == "" {
+		log.Warn().Msg("Missing username or password")
+		writeErrorResponse(w, http.StatusBadRequest, "Username and password are required")
+		return
+	}
+
+	// Authenticate user and get JWT token
+	token, err := h.authService.Login(req.Username, req.Password)
+	if err != nil {
+		// Don't reveal wether user or pass was wrong
+		log.Warn().Str("Username", req.Username).Err(err).Msg("Login failed")
+		writeErrorResponse(w, http.StatusUnauthorized, "Invalid username or password")
+		return
+	}
+
+	// Get user info for response
+	user, err := h.db.GetUserByUsername(req.Username)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get user after login")
+		writeErrorResponse(w, http.StatusInternalServerError, "Login successful but failed to retrieve user info")
+		return
+	}
+
+	// Create response
+	response := model.AuthResponse{
+		Token: token,
+		User: model.UserSummary{
+			UserID:   user.ID,
+			Username: user.Username,
+			Role:     user.Role,
+		},
+	}
+
+	log.Info().Str("Username", user.Username).Int("User ID", user.ID).Msg("User logged in succesfully")
+	writeJSONResponse(w, http.StatusOK, response)
+}
+
+// GET /api/auth/me - GET current user handler
