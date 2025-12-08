4b5133f3156a19dbb720bfeac23fac15223bb5f9
Kevin Rosengren <rosenkev4@gmail.com>
2025-12-08 02:51:09 -0600

    Created file for authentication models for registration request, login request, auth response, and safe user data

---
+++ internal/model/auth_models.go
@@ -0,0 +1,29 @@
+package model
+
+// Registration request body
+type RegisterRequest struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+// Login request body
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Authentication response
+type AuthResponse struct {
+	Token   string      `json:"token"`
+	User    UserSummary `json:"user"`
+	Profile interface{} `json:"profile"`
+}
+
+// Safe user data (no password)
+type UserSummary struct {
+	UserID   int    `json:"user_id"`
+	Username string `json:"username"`
+	Role     string `json:"role"`
+}
